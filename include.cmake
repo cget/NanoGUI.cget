ec0b408316bbd2db186f6a61331a2627ce06f533
Justin <j.david.berger@gmail.com>
2017-02-05 20:58:29 -0700

    Added find cmake file

--- include.cmake
+++ include.cmake
@@ -1,1 +1,2 @@
-CGET_HAS_DEPENDENCY(NanoGUI GITHUB wjakob/nanogui VERSION master)
+file(COPY "${REPO_DIR}/FindNANOGUI.cmake" DESTINATION "${CGET_INSTALL_DIR}/lib/cmake")
+CGET_HAS_DEPENDENCY(NANOGUI GITHUB wjakob/nanogui VERSION master)
